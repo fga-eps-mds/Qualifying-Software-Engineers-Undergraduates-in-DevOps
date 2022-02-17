0dfc898e6d989af01acf0b63baba94bc5e796ed1
Carla Rocha <seu_email@exemplo.com>
2022-02-17 10:53:31 -0300

    mudando a estrutura

--- praticas/src/beatutifulsoup-basic.py
+++ praticas/src/beatutifulsoup-basic.py
@@ -1,13 +1,16 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+def playingWebScraping():
+    html = urlopen("https://mofanpy.com/static/scraping/basic-structure.html").read().decode('utf-8')
+
+    soup = BeautifulSoup(html, features='lxml')
+    print(soup.h1)
+    print('\n', soup.p)
+
+    all_href = soup.find_all('a')
+    all_href = [l['href'] for l in all_href]
+    print('\n', all_href)
+
 # if has Chinese, apply decode()
-html = urlopen("https://mofanpy.com/static/scraping/basic-structure.html").read().decode('utf-8')
-
-soup = BeautifulSoup(html, features='lxml')
-print(soup.h1)
-print('\n', soup.p)
-
-all_href = soup.find_all('a')
-all_href = [l['href'] for l in all_href]
-print('\n', all_href)
+playingWebScraping();
